7054f831849053dff9302ce8f1463b12593e2b9f
AlexDv <DvoinikovAV@yandex.ru>
2024-01-29 20:01:48 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args)
     {
+        //Задание 1
         String firstName="Иван";
         String middleName="Иванович";
         String lastName="Иванов";
         String fullName=lastName+" "+firstName+" "+middleName;
         System.out.println("Ф.И.О. сотрудника - "+fullName);
+
+        //Задание 2
+        String upperName=fullName.toUpperCase();
+        System.out.println("ФДанные Ф. И. О. сотрудника для заполнения отчета — "+upperName);
     }
 }
